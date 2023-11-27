68aa8787bb88471f5d9863076450a7aed5806442
Md. Fazle Rabbi Riyad <riyadd78666@gmail.com>
2023-11-28 01:38:22 +0600

    Some more solved problem

--- problem_263A.py
+++ problem_263A.py
@@ -3,7 +3,6 @@ matrix = [list(map(int, input().split())) for _ in range(5)]
 for i in range(5):
     for j in range(5):
         if matrix[i][j] == 1:
-            moves = abs(i - 3)+abs(j - 3)
+            moves = abs(i - 3) + abs(j - 3)
 
 print(moves)
-
